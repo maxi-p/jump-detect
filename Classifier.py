e03eb4bab227095b19bdfb81599674eedc85ab92
Maksim Petrushin <petrushin.maksim001@gmail.com>
2024-06-30 18:24:13 -0400

    app clips individual jumps with 70-80% accuracy

--- Classifier.py
+++ Classifier.py
@@ -7,12 +7,28 @@ from tensorflow.keras.layers import Conv2D, MaxPooling2D, Dense, Flatten
 from tensorflow.keras.models import load_model
 from tensorflow.keras.metrics import Precision, Recall, BinaryAccuracy
 
+RELEASED, DESELECT = 1,1
+gx1, gx2, gy1, gy2, gxTemp, gyTemp, PRESSED, SELECT = 0,0,0,0,0,0,0,0
+state = RELEASED
+selection_state = DESELECT
+
 class Classifier:
-    def __init__(self,videoPath, configPath, modelPath, classesPath):
+    def __init__(self,videoPath, configPath, modelPath, classesPath, videoName):
         self.videoPath = videoPath
         self.configPath = configPath
         self.modelPath = modelPath
         self.classesPath = classesPath
+        self.height = 0
+        self.width = 0
+        self.videoName = ''
+        self.codec = cv2.VideoWriter_fourcc(*'MP4V')
+
+        for char in videoName:
+            if char != '.':
+                self.videoName += char
+            else:
+                break
+
 
         self.net = cv2.dnn_DetectionModel(self.modelPath, self.configPath)
         self.net.setInputSize(320, 320)
@@ -21,7 +37,7 @@ class Classifier:
         self.net.setInputSwapRB(True)
         self.readClasses()
 
-        self.air = load_model(os.path.join('models', 'imageclassifier.h5'))
+        # self.air = load_model(os.path.join('models', 'imageclassifier.h5'))
         self.state = ['air', 'land']
 
         self.font = cv2.FONT_HERSHEY_SIMPLEX
@@ -30,17 +46,35 @@ class Classifier:
         with open(self.classesPath, 'r') as f:
             self.classesList = f.read().splitlines()
 
-        self.classesList.insert(0, '__Background__')
-        
+        self.classesList.insert(0, '__Background__')  
+
+    def createDir(self, dirname):
+        dirs = os.listdir('./clips/')
+        for dir in dirs:
+            if dir == dirname:
+                return
+        os.mkdir('./clips/'+dirname)
 
     def onVideo(self):
+        global selection_state
+        
+        self.createDir(self.videoName)
+        
         cap = cv2.VideoCapture(self.videoPath)
-
-        if (cap.isOpened() == False):
+        recordingCap = cv2.VideoCapture(self.videoPath)
+        
+        if cap.isOpened() == False or recordingCap.isOpened() == False:
             print("error")
             return 
             
         (success, image) = cap.read()
+        self.height = image.shape[0]
+        self.width = image.shape[1]
+        recordingSuccess, recordingImage, outClip = None, None, None
+        endFrame = 0
+        writeCounter = 1
+        frame = 0
+        recordingFrame = -29
 
         while success:
             classLabelIDs, confidences, bboxs = self.net.detect(image, confThreshold = 0.3)
@@ -51,6 +85,14 @@ class Classifier:
 
             bboxIdx = cv2.dnn.NMSBoxes(bboxs, confidences, score_threshold= 0.5, nms_threshold= 0.2)
 
+            if endFrame != 0:
+                if recordingFrame == endFrame:
+                    endFrame = 0
+                    # write the clip
+                else:
+                    # add frame to out
+                    outClip.write(recordingImage)
+        
             if len(bboxIdx) != 0:
                 for i in range(0, len(bboxIdx)):
                     if (classLabelIDs[i] == 1) :
@@ -70,20 +112,79 @@ class Classifier:
                         x_start = max(0, x)
                         x_end = min(image.shape[1], x+w)
 
-                        cropped_image = image[y_start:y_end, x_start:x_end]
-                        resize = tf.image.resize(cropped_image, (256,256))
-                        yhat = self.air.predict(np.expand_dims(resize/255, 0))
-                        if yhat > 0.3:
-                            cv2.putText(image, 'land', (10,450), self.font, 3, (0, 0, 255), 2, cv2.LINE_AA)
-                        else:
+                        if inBox(x_start, y_start, x_end, y_end, gx1, gy1, gx2, gy2):
+                            if endFrame == 0:
+                                endFrame = frame + 30
+                                outClip = cv2.VideoWriter('./clips/'+self.videoName+'/CLIP-'+str(writeCounter)+'.mp4', self.codec, 30, (self.width, self.height))
+                                writeCounter += 1
                             cv2.putText(image, 'AIR', (10,450), self.font, 3, (0, 255, 0), 2, cv2.LINE_AA)
-                        cv2.rectangle(image, (x_start,y_start), (x_end, y_end), color=(255,255,255), thickness=1)
+                            cv2.rectangle(image, (x_start,y_start), (x_end, y_end), color=(255,255,255), thickness=2)
 
-            cv2.imshow("Result", image)
+            cv2.rectangle(image, (gx1, gy1), (gx2, gy2), color=(255,255,0), thickness=2)
+            cv2.imshow('scan', image)
+            cv2.setMouseCallback('scan', mouseCallback)
 
             key = cv2.waitKey(1) & 0xFF
             if key == ord('q'):
                 break
+            if key == ord('p'):
+                cv2.waitKey(-1)
+            if key == ord('s'):
+                selection_state = SELECT
+            if key == ord('d'):
+                selection_state = DESELECT
 
             (success, image) = cap.read()
-        cv2.destroyAllWindows()     
+            if recordingFrame >= 0:
+                (recordingSuccess, recordingImage) = recordingCap.read()
+            
+            frame += 1
+            recordingFrame += 1
+        cv2.destroyAllWindows()   
+
+def mouseCallback(event, x, y, flags, params):
+    global gx1, gy1, gx2, gy2, state, selection_state
+    if selection_state == SELECT:
+        if state == PRESSED:
+            gx2 = x
+            gy2 = y
+        if event == cv2.EVENT_LBUTTONDOWN:
+            gx1 = x
+            gy1 = y
+            gx2 = x
+            gy2 = y
+            state = PRESSED
+        if event == cv2.EVENT_LBUTTONUP:
+            state = RELEASED
+
+def inBox(ax1, ay1, ax2, ay2, bx1, by1, bx2, by2):
+    w1 = abs(ax2 - ax1)
+    w2 = abs(bx2 - bx1)
+    h1 = abs(ay2 - ay1)
+    h2 = abs(by2 - by1)
+    
+    if ax1 < ax2:
+        mx1 = ax1+int(w1/2)
+    else:
+        mx1 = ax2+int(w1/2)
+    if ay1 < ay2:
+        my1 = ay1+int(h1/2)
+    else:
+        my1 = ay2+int(h1/2)
+    if bx1 < bx2:
+        mx2 = bx1+int(w2/2)
+    else:
+        mx2 = bx2+int(w2/2)
+    if by1 < by2:        
+        my2 = by1+int(h2/2)
+    else:
+        my2 = by2+int(h2/2)
+
+    dx = abs(mx1 - mx2)
+    dy = abs(my1 - my2)
+
+    if (dx < int((w1 + w2)/2)) and (dy < int((h1 + h2)/2)):
+        return True
+    
+    return False
+
--- test.py
+++ test.py
@@ -1,14 +1,15 @@
 from Classifier import *
 import os
+import sys
 
 def main():
-    videoPath = os.path.join('test', 'big_test.mp4')
-
+    videoName = sys.argv[1]
+    videoPath = os.path.join('test', videoName)
     configPath = os.path.join('model_data', 'ssd_mobilenet_v3_large_coco_2020_01_14.pbtxt')
     modelPath = os.path.join('model_data', 'frozen_inference_graph.pb')
     classesPath = os.path.join('model_data', 'coco.names')
 
-    classifier = Classifier(videoPath, configPath, modelPath, classesPath)
+    classifier = Classifier(videoPath, configPath, modelPath, classesPath, videoName)
     classifier.onVideo()
 
 if __name__ == '__main__':
